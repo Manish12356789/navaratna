69c5f4ad5a7db393e59b375d6aa1bcc876ca3837
Manish12356789 <manishparajuli2018@gmail.com>
2022-01-11 11:14:15 +0545

    initial added

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+//fixed navbar 
+// const navbar = document.querySelector(".navbar");
+// console.log("navbar:",navbar)
+// window.onscroll = ()=>{
+//   this.scrollY > 250 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
+// }
+
+// MDB Lightbox Init
+$(function () {
+  $("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
+  });
+
+// console.log("scroll", window.scrollY);
